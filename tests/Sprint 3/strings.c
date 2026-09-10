6d75866b447844118d142b0dabea81c5845d95f9
arthursouto09 <151585884+arthursouto09@users.noreply.github.com>
2026-09-10 18:27:13 -0300

    Add strings.c with various string examples
    Teste para strings

---
+++ tests/Sprint 3/strings.c
@@ -0,0 +1,15 @@
+"hello"
+"a"
+""
+"com espacos no meio"
+
+"aspas escapadas: \" dentro"
+"barra invertida: \\ escapada"
+"tab\tembutido"
+"quebra\nde linha embutida"
+
+"123"
+"true"
+"!@#$%^&*()"
+
+"string nao fechada
